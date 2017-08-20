b047cd0aa9c72ff03422f5fc923380a08363230a
BadWaka <1456683844@qq.com>
2017-08-20 21:44:20 +0800

    index

--- demo/ejsDemo.js
+++ demo/ejsDemo.js
@@ -1,5 +1,5 @@
 const ejs = require('ejs');
 
-const people = ['111','222','333'];
-const html = ejs.render('<%= people.join(","); %>',{people:people});
+const people = ['111', '222', '333'];
+const html = ejs.render('<%= people.join(","); %>', {people: people});
 console.log(`html${html}`);
--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
 
 const app = express();
 
-app.get('/',function(req,res){
-    res.send('Hello World');
+// 设置静态资源目录
+app.use(express.static('public'));
+
+// 设置模板
+app.set('views', path.join(__dirname, 'views')); // 放模板文件的目录
+app.engine('html', ejs.renderFile); // 更改后缀名
+app.set('view engine', 'html'); // 模板引擎
+
+app.get('/', function (req, res) {
+    res.render('index');
 });
 
-const server = app.listen(3000,function(){
-    const host  = server.address().address;
-    const port = server.address().port;
+const server = app.listen(3000, function () {
+    const host = server
+        .address()
+        .address;
+    const port = server
+        .address()
+        .port;
 
     console.log(`Listening at http://${host}:${port}`);
 });
