f8950ef312780dfecfcc061c3045f0dcca88d24e
BadWaka <1456683844@qq.com>
2017-08-22 17:28:02 +0800

    初步更改至koa

--- server.js
+++ server.js
@@ -1,28 +1,47 @@
-const express = require('express');
+/************************ require ************************/
+
+const Koa = require('koa');
 const path = require('path');
-const ejs = require('ejs');
+const console = require('tracer').colorConsole(); // 增强console
+const koaStatic = require('koa-static'); // koa-static   设置静态资源目录
+const koaMount = require('koa-mount'); // koa-mount 将中间件挂载到特定url下
+const koaEjs = require('koa-ejs');
+const koaBodyParser = require('koa-bodyparser'); // koa-bodyparser 解析post中的data
 
-const app = express();
+/************************ initial ************************/
 
-// 设置静态资源目录
-app.use('/static', express.static('static'));
+const app = new Koa();
 
-// 设置模板
-app.set('views', path.join(__dirname, 'views')); // 放模板文件的目录
-app.engine('html', ejs.renderFile); // 更改后缀名
-app.set('view engine', 'html'); // 模板引擎
+// 是否是生产环境
+const isProduction = process.env.NODE_ENV === 'production';
+console.debug('当前环境 process.env.NODE_ENV', process.env.NODE_ENV);
 
-app.get('/', function (req, res) {
-    res.render('index');
+/************************ middleware ************************/
+
+// 设置静态目录，使用 koa-mount 和 koa-static 将静态资源目录挂载到 /static 路径下
+app.use(koaMount('/static', koaStatic('static')));
+
+// 设置ejs中间件
+koaEjs(app, {
+    root: path.join(__dirname, 'views'),
+    layout: false,
+    viewExt: 'ejs',
+    cache: false,
+    debug: false
 });
 
-const server = app.listen(3000, function () {
-    const host = server
-        .address()
-        .address;
-    const port = server
-        .address()
-        .port;
+// 解析post的data
+app.use(koaBodyParser());
 
-    console.log(`Listening at http://${host}:${port}`);
-});
+// app.use(async (ctx, next) => {
+//     ctx.body = 'Hello World';
+// });
+
+/************************ start server ************************/
+
+app.listen(3000);
+console.debug('listening 3000...');
+
+/************************ error handler ************************/
+
+app.on('error', err => console.error('server error', err));
