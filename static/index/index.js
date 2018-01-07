e34b39909094a13ab4a1194b5d7116305c21118c
BadWaka <1456683844@qq.com>
2018-01-07 22:54:58 +0800

    一切归零

--- static/index/index.js
+++
@@ -1,7 +0,0 @@
-var swiper = new Swiper('.swiper-container', {
-    speed: 3000,
-    autoplay: 3000,
-    autoplayDisableOnInteraction: false,
-    effect: 'fade',
-    loop: true,
-});
--- utils/constant.js
+++
@@ -1,9 +0,0 @@
-/**
- * 常量
- */
-
-const constant = {
-    cookieName: 'waka_website', // 写在浏览器中的cookie名
-};
-
-module.exports = constant;
